a0ca5b792536cf64e52e6c8fbcb74d4f15c3fe98
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-04-11 15:30:11 +0900

    setting d3 in react

---
+++ react-d3/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState,  useEffect } from 'react';
+import * as d3 from "d3";
+import "./App.css";
+import { message } from './message';
+
+const csvUrl = 'https://gist.githubusercontent.com/BoKyeong-Kim/ac522f3205103f1db0268505d2febd46/raw/8437c5d2fc446bc04a76e330cfba0fb99c5c2084/cssNamedColors.csv'
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    d3.csv(csvUrl).then(setData);
+  }, []);
+
+  return <div className='pre'>Data is {data ? message(data) : 'loading....'}</div>;
+};
+
+export default App;
