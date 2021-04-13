83d36a55c9f7e673bf9554dabc4b659931da4373
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-04-13 19:34:56 +0900

    create pie chart - use arc function in d3

--- react-d3/src/App.js
+++ react-d3/src/App.js
@@ -5,6 +5,15 @@ import { message } from './message';
 
 const csvUrl = 'https://gist.githubusercontent.com/BoKyeong-Kim/ac522f3205103f1db0268505d2febd46/raw/8437c5d2fc446bc04a76e330cfba0fb99c5c2084/cssNamedColors.csv'
 
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+
+const pieArc = d3.arc()
+  .innerRadius(0)
+  .outerRadius(width);
+
 const App = () => {
   const [data, setData] = useState(null);
 
@@ -12,7 +21,21 @@ const App = () => {
     d3.csv(csvUrl).then(setData);
   }, []);
 
-  return <div className='pre'>Data is<br/> {data ? message(data) : 'loading....'}</div>;
-};
+  if(!data) {
+    return <pre>loading...</pre>
+  }
+
+  return <svg width ={width} height={height}>
+    <g transform={`translate(${centerX}, ${centerY})`}>
+      {data.map((d,i) => 
+      <path fill ={d['RGB hex value']} 
+      d={pieArc({
+        startAngle : i / data.length * 2 * Math.PI,
+        endAngle : (i + 1) / data.length * 2 * Math.PI
+      })}
+      />)}
+      </g>
+  </svg>
+  };
 
 export default App;
