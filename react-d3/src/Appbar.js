b4cccc620efeb519a5f809c8651225c2ef12ef98
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-04-20 16:39:53 +0900

    add const margin about backgroud

--- react-d3/src/Appbar.js
+++ react-d3/src/Appbar.js
@@ -5,6 +5,7 @@ import "./App.css";
 const csvUrl = 'https://gist.githubusercontent.com/BoKyeong-Kim/d39d56276edefd0999731e76dbdcb805/raw/d08ce4d95e4bf94261c53d3490deb555f357be36/UN_Population_2019.csv'
 const width = 960;
 const height = 500;
+const margin = { top : 50 , right : 50,  bottom : 50 , left : 50 };
 
 const App = () => {
   const [data, setData] = useState(null);
@@ -25,21 +26,25 @@ const App = () => {
     return <pre>loading...</pre>
   }
 
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
 
   const yScale = d3.scaleBand()
                  .domain(data.map(d => d.Country))
-                 .range([0, height]);
+                 .range([0, innerHeight]);
   
   const xScale = d3.scaleLinear()
                 .domain([0, d3.max(data.map(d => d.Population))])
-                .range([0, width]);
+                .range([0, innerWidth]);
 
   return <svg width ={width} height={height}>
+    <g transform= {`translate(${margin.left}, ${margin.right})`}>
     {data.map(d => (
           <rect x={0} 
                 y={yScale(d.Country)} 
                 width = {xScale(d.Population)}
                 height = {yScale.bandwidth()} />))}
+    </g>
   </svg>
 
   };
