8a95f46797e878709e73094a5335d1b8eddef3f0
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-04-25 01:39:48 +0900

    refactoring - create Marks component

--- react-d3/src/Appbar.js
+++ react-d3/src/Appbar.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { useData } from './useData';
 import { AxisBottom } from './AxisBottom';
 import { AxisLeft } from './AxisLeft';
+import { Marks } from './Marks';
 
 const width = 960;
 const height = 500;
@@ -34,12 +35,7 @@ const App = () => {
     <g transform= {`translate(${margin.left}, ${margin.top})`}>
         <AxisBottom xScale={xScale} innerHeight={innerHeight} />
         <AxisLeft yScale={yScale}/>
-    {data.map(d => (
-          <rect key ={d.Country}
-                x={0} 
-                y={yScale(d.Country)} 
-                width = {xScale(d.Population)}
-    height = {yScale.bandwidth()} />))}
+        <Marks data={data} xScale={xScale} yScale={yScale}/>
       </g>
       </svg>
   )};
