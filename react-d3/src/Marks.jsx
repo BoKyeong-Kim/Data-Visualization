945861b117d9782a973ceaabc41c5a9c91f43ead
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-04-28 16:45:18 +0900

    add bar chart color code about bar

--- react-d3/src/Marks.jsx
+++ react-d3/src/Marks.jsx
@@ -1,6 +1,6 @@
 export const Marks = ({ data, xScale, yScale, xValue, yValue }) => 
  data.map(d => (
-    <rect key ={yValue(d)}
+    <rect className="mark" key ={yValue(d)}
           x={0} 
           y={yScale(yValue(d))} 
           width = {xScale(xValue(d))}
