c08f8b94f77c62c2f120d24eeaaff67f319603de
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-05-25 22:48:42 +0900

    create AppMenu component - useState

--- react-d3/src/AppMenu.js
+++ react-d3/src/AppMenu.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 
+const Dropdown = ({options, id, selectedValue, onselectionchange}) => (
+  <select id={id} onChange={event => onselectionchange(event.target.value)}>
+    {options.map(({ value, label }) => (
+      <option value={value} selected={value === selectedValue}>
+        {label}
+       </option>
+    ))}
+  </select>
+)
+
+const options = [
+  { value : 'dog', label : 'Dog'},
+  { value : 'cat', label : 'Cat'},
+  { value : 'hamster', label : 'Hamster'},
+  { value : 'parrot', label : 'Parrot'},
+  { value : 'spider', label : 'Spider'},
+  { value : 'goldfish', label : 'Goldfish'}
+];
 
 const App = () => {
+  const [selectedValue, setSelectedValue] = useState();
+  console.log(selectedValue)
   return (
     <>
     <div>
-    <label for="pet-select">Choose a pet:</label>
-    <select name="pets" id="pet-select">
-        <option value="">--Please choose an option--</option>
-        <option value="dog">Dog</option>
-        <option value="cat">Cat</option>
-        <option value="hamster">Hamster</option>
-        <option value="parrot">Parrot</option>
-        <option value="spider">Spider</option>
-        <option value="goldfish">Goldfish</option>
-    </select>
+    <label for="pet-select">Choose a pet : </label>
+      <Dropdown 
+        options={options} 
+        id="pet-select"
+        selectedValue={selectedValue}
+        onselectionchange={setSelectedValue}
+      />
     </div>
     </>
 )};
